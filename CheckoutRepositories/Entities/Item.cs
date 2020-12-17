1ff2225577557a97e4ddc4fc78079df9a0f63ee6
Warren Lockhart <wlockhart55@gmail.com>
2020-12-17 17:33:21 +0000

    Populate item store

--- CheckoutRepositories/Entities/Item.cs
+++ CheckoutRepositories/Entities/Item.cs
@@ -5,5 +5,7 @@
         public string Name { get; set; }
 
         public double Price { get; set; }
+
+        public Offer Offer { get; set; }
     }
 }
--- CheckoutRepositories/ItemsDataStore.cs
+++ CheckoutRepositories/ItemsDataStore.cs
@@ -5,9 +5,13 @@ namespace CheckoutRepositories
 {
     public static class ItemsDataStore
     {
-        public static IDictionary<string, Item> Items = new Dictionary<string, Item>()
+        public static IEnumerable<Item> Items = new List<Item>()
         {
-
+            new Item { Name = "freddo", Price = 0.5, Offer = new Offer { Quantity = 2, Price = 0.4 }},
+            new Item { Name = "apple", Price = 0.6 },
+            new Item { Name = "banana", Price = 0.6, Offer = new Offer { Quantity = 2, Price = 1.0 }},
+            new Item { Name = "orange", Price = 0.4, Offer = new Offer { Quantity = 3, Price = 0.9 }},
+            new Item { Name = "curly wurly", Price = 0.7 }
         };
     }
 }
