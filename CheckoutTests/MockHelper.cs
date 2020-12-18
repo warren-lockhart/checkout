80520522da5c46ea233575f0daecd4ff111ed44b
Warren Lockhart <wlockhart55@gmail.com>
2020-12-18 17:04:54 +0000

    Clean up test setup with mock helper

---
+++ CheckoutTests/MockHelper.cs
@@ -0,0 +1,49 @@
+﻿using CheckoutRepositories;
+using CheckoutRepositories.Entities;
+using Moq;
+using System.Collections.Generic;
+
+namespace CheckoutTests
+{
+    public static class MockHelper
+    {
+        public static Mock<IDataStore> GetMockDataStore()
+        {
+            var mockDataStore = new Mock<IDataStore>();
+
+            mockDataStore.Setup(r => r.ItemCheck(It.IsAny<string>())).Returns((string item) =>
+            {
+                var expectedItems = new List<string>() { "orange", "apple", "freddo" };
+
+                if (expectedItems.Contains(item))
+                {
+                    return true;
+                }
+
+                return false;
+            });
+
+            mockDataStore.Setup(r => r.Get(It.IsAny<string>())).Returns((string item) =>
+            {
+                if (item == "orange")
+                {
+                    return new Item { Name = "orange", Price = 0.4M, Offer = new Offer { Quantity = 3, Price = 0.9M } };
+                }
+
+                if (item == "apple")
+                {
+                    return new Item { Name = "apple", Price = 0.6M };
+                }
+
+                if (item == "freddo")
+                {
+                    return new Item { Name = "freddo", Price = 0.5M, Offer = new Offer { Quantity = 2, Price = 0.8M } };
+                }
+
+                return new Item();
+            });
+
+            return mockDataStore;
+        }
+    }
+}
--- CheckoutTests/ScanServiceTests.cs
+++ CheckoutTests/ScanServiceTests.cs
@@ -1,9 +1,7 @@
 using CheckoutRepositories;
-using CheckoutRepositories.Entities;
 using CheckoutServices;
 using Moq;
 using System;
-using System.Collections.Generic;
 using Xunit;
 
 namespace CheckoutTests
@@ -17,40 +15,8 @@ namespace CheckoutTests
 
         public ScanServiceTests()
         {
-            _mockDataStore = new Mock<IDataStore>();
+            _mockDataStore = MockHelper.GetMockDataStore();
             _shoppingList = new ShoppingList();
-
-            _mockDataStore.Setup(r => r.ItemCheck(It.IsAny<string>())).Returns((string item) =>
-            {
-                var expectedItems = new List<string>() { "orange", "apple", "freddo" };
-
-                if (expectedItems.Contains(item))
-                {
-                    return true;
-                }
-
-                return false;
-            });
-
-            _mockDataStore.Setup(r => r.Get(It.IsAny<string>())).Returns((string item) =>
-            {
-                if (item == "orange")
-                {
-                    return new Item { Name = "orange", Price = 0.4M, Offer = new Offer { Quantity = 3, Price = 0.9M } };
-                }
-
-                if (item == "apple")
-                {
-                    return new Item { Name = "apple", Price = 0.6M };
-                }
-
-                if (item == "freddo")
-                {
-                    return new Item { Name = "freddo", Price = 0.5M, Offer = new Offer { Quantity = 2, Price = 0.8M } };
-                }
-
-                return new Item();
-            });
 
             _scanService = new ScanService(_mockDataStore.Object, _shoppingList);
         }
